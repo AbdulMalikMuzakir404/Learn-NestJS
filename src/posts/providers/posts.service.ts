6eddcbb68c5805c53dfbd247b53f8f4e2c37bd71
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-06-08 23:48:53 +0700

    Integrate UsersModule into PostsModule and update PostsService to utilize UsersService for user data retrieval.

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../../users/providers/users.service';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly usersService: UsersService) {}
+
   public findAll(userId: string) {
+    const user = this.usersService.findOneById(userId);
+
     return [
       {
+        user: user,
         title: 'test title',
         content: 'test content',
       },
       {
+        user: user,
         title: 'test title',
         content: 'test content',
       },
